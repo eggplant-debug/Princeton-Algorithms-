2ae794e17a85a18ca16bc5831edc4e6ac5e06a6f
eggplant-debug <18391458847@163.com>
2021-03-17 00:50:04 +0800

    # 1

--- Percolation.java
+++ Percolation.java
@@ -23,39 +23,39 @@ public class Percolation {
     }
     // opens the site (row, col) if it is not open already
     //还需考虑四周连接的水块
-    public void open(int row, int col) {
-        valid(row,col);
-        if(isOpen(row,col)){
-            return;
-        }
-        else{
-            a[row*N+col]=1;
-            //连接周围四个地块
-//            if(isOpen)
-        }
-    }
+//    public void open(int row, int col) {
+//        valid(row,col);
+//        if(isOpen(row,col)){
+//            return;
+//        }
+//        else{
+//            a[row*N+col]=1;
+//            //连接周围四个地块
+////            if(isOpen)
+//        }
+//    }
 
     // is the site (row, col) open?
-    public boolean isOpen(int row, int col){
-
-    }
+//    public boolean isOpen(int row, int col){
+//
+//    }
 
     // is the site (row, col) full?
     // 要考虑是否连通到了上面,从这个地块能否连通到最上的地块
-    public boolean isFull(int row, int col){
-
-    }
+//    public boolean isFull(int row, int col){
+//
+//    }
 
     // returns the number of open sites
-    public int numberOfOpenSites(){
-
-    }
+//    public int numberOfOpenSites(){
+//
+//    }
 
     // does the system percolate?
     //
-    public boolean percolates(){
-
-    }
+//    public boolean percolates(){
+//
+//    }
 
     // test client (optional)
     public static void main(String[] args) {
